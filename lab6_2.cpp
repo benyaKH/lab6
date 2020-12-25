a7f6e0a78e845df5544064521566b7c73f711e03
lab314-17@labeng.local <lab314-17@labeng.local>
2020-12-25 16:29:26 +0700

    update_codefahsai

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,5 +1,42 @@
 //Dialog of the first example is given below.
+#include <iostream>
+using namespace std ;
 
+int main() {
+    string name ;
+    cout<<"Fahsai: Sawadee ka...Can you tell me your name?\n" ;
+    cout<<"?????: " ;
+    getline(cin,name) ;
+    cout<<"Fahsai: Wow!!! "<< name << " is a really cool name.\n" ;
+
+
+    int id ;
+	cout<<"Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout<<name<<": " ;
+    cin>>id;
+    id=(id/10000000)-12 ;
+    cout<<"Fahsai: I think you may be GEAR "<<id<<". I have a free movie ticket for you.\n" ;
+    cin.ignore();
+
+    string movie ;
+    cout<<"Fahsai: Let's go to the cinema together!!!\n" <<"Fahsai: What movie do you want to watch?\n" ;
+    cout<<name<<": " ;
+    getline(cin,movie) ;
+
+    string day ;
+    cout<<"Fahsai: So....which day are you free to go with me?\n" ;
+    cout<<name<<": " ;
+    getline(cin,day) ;
+    cout<<"Fahsai: "<< day <<"....that is OK!!! I'm looking forward to watching "<<movie<<" with you.\n" ;
+    
+
+    string text ;
+    cout<<name<<": " ;
+    cin>>text ;
+    cout<<"Fahsai: 555+ see you "<<day<<". Bye Bye \\(^ ^)/" ;
+
+
+    ret
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
 ?????: Luke Skywalker
